2ca12da7e376a512cdd83a8960546bf1bebcd1a7
Edwin Cardenas <eacardenase@unal.edu.co>
2023-05-03 16:25:09 -0500

    feat remove code duplication with component

--- app/Http/Controllers/ExampleController.php
+++ app/Http/Controllers/ExampleController.php
@@ -7,19 +7,10 @@ use Illuminate\Http\Request;
 class ExampleController extends Controller
 {
     public function homePage() {
-        // imagine we loaded data from the database
-        $myName = "Edwin";
-        $catName = 'Mango';
-        $animals = ["Mango", "Apollo", "Coco"];
-
-        return view('homepage', [
-            'name' => $myName,
-            'catName' => $catName,
-            'animalsArray' => $animals
-        ]);
+        return view('homepage');
     }
 
     public function aboutPage() {
-        return '<h1>About page!</h1><a href="/">Back to home</a>';
+        return view('single-post');
     }
 }
