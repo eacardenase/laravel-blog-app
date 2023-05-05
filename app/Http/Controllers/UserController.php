2fd4ed895a119c32359f2192f55e202a434d3ee2
Edwin Cardenas <eacardenase@unal.edu.co>
2023-05-05 11:24:47 -0500

    feat: add validation to register form

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,15 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
     public function register(Request $request)
     {
         $incomingField = $request->validate([
-            'username' => 'required',
-            'email' => 'required',
-            'password' => 'required'
+            'username' => ['required', 'min:3', 'max:20', Rule::unique('users', 'username')],
+            'email' => ['required', 'email', Rule::unique('users', 'email')],
+            'password' => ['required', 'min:8', 'confirmed']
         ]);
 
         User::create($incomingField);
